51d2beb1ecbdabb07c06fec5524ce0c6cbfcb6f3
Chandrakant Kushwah <chandrakantkushwah@Chandrakants-MacBook-Air.local>
2023-11-06 12:22:15 +0530

    added landing page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,8 +9,8 @@ import NotificationCardsSection from "../components/NotificationCardsSection";
 
 const Home = () => {
   return (
-    <div className="w-screen md:h-screen flex xl:flex-row flex-col bg-black xl:overflow-hidden">
-      <main className="order-1 gradient xl:w-[calc(100%-44.44%)] 2xl:flex bg-black 2xl:items-center w-full sm:py-10 py-9 lg:h-screen min-h-screen overflow-y-scroll overflow-x-hidden">
+    <div className="w-screen md:h-screen min-h-screen flex xl:flex-row flex-col bg-black xl:overflow-hidden">
+      <main className="order-1 gradient xl:w-[calc(100%-44.44%)] 2xl:flex bg-black 2xl:items-center my-auto w-full sm:py-10 py-9 lg:h-screen min-h-screen overflow-y-scroll overflow-x-hidden">
         <div className="w-full">
           <Section1 />
           <Section2 />
@@ -27,14 +27,29 @@ const Section1 = () => {
   return (
     <section className="flex lg:flex-row flex-col lg:gap-15 gap-9 sm:pl-[3.75rem] pl-9 lg:pr-0">
       <div className="text-[#F2F2F2] lg:w-[46%] flex flex-col gap-4 lg:pr-0 sm:pr-[3.75rem] pr-5">
-        <motion.div initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, delay: 0.0 }}>
+        <motion.div
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 0.0 }}
+        >
           <img src={Bell} alt="bell" className="w-8 h-8" />
         </motion.div>
-        <motion.h2 initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, delay: 0.2 }} className="text-[1.938rem] font-medium text-shadow-notification leading-9">
+        <motion.h2
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 0.2 }}
+          className="text-[1.938rem] font-medium text-shadow-notification leading-9"
+        >
           Get notified when a highly correlated whale makes a move
         </motion.h2>
-        <motion.p initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, delay: 0.4 }} className="opacity-70 font-medium">
-          Find out when a certain whale moves more than any preset amount on-chain or when a dormant whale you care about becomes active.
+        <motion.p
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 0.4 }}
+          className="opacity-70 font-medium"
+        >
+          Find out when a certain whale moves more than any preset amount
+          on-chain or when a dormant whale you care about becomes active.
         </motion.p>
       </div>
       <div className="lg:w-[54%] w-full">
@@ -47,18 +62,44 @@ const Section1 = () => {
 const Section2 = () => {
   return (
     <section className="flex md:flex-row flex-col sm:px-[3.75rem] px-9 md:pt-[4.5rem] pt-9 gap-8">
-      <motion.div initial={{ opacity: 0, x: -100 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 1, delay: 0.8 }} className="image">
-        <img src={"https://s3-alpha-sig.figma.com/img/a837/e66b/317b128c03dcf7840ae72269ac076405?Expires=1699833600&Signature=p18wd3dXPhaNTgmqrqTiEE8C4ccmhSoCqTxJRMOArT8RDqncCTH70MgOPy8xJKwhsrLtZkBBEApVEpy8axSlYqBh4SIrsSulSd0ih-jNr2HHVq36uiqNY6gcRloIacn8tz~CFywwzE3YUoEhd0Tz5Y3Lqr9zaSlkiQRwq9ZQyRRZEAtcTuCgEqha633WwPPBayx5XmLdM2qWFCTO4ODKD8iMrycGf5d1K5wMosqbl5TciWi81NcigySgc9g0ALtIi0pdAN25rezZipc7Kq~m6VFjf-KKt9UWRf9Z8tBo3KAGUkqeB6XX2bpm8~yiv~uehnmzbGlAnxyPnLYsvdS9Yw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"} alt="cohorts" className="md:w-[21rem] md:h-[19.125rem] rounded-xl" />
+      <motion.div
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1, delay: 0.8 }}
+        className="image"
+      >
+        <img
+          src={
+            "https://s3-alpha-sig.figma.com/img/a837/e66b/317b128c03dcf7840ae72269ac076405?Expires=1699833600&Signature=p18wd3dXPhaNTgmqrqTiEE8C4ccmhSoCqTxJRMOArT8RDqncCTH70MgOPy8xJKwhsrLtZkBBEApVEpy8axSlYqBh4SIrsSulSd0ih-jNr2HHVq36uiqNY6gcRloIacn8tz~CFywwzE3YUoEhd0Tz5Y3Lqr9zaSlkiQRwq9ZQyRRZEAtcTuCgEqha633WwPPBayx5XmLdM2qWFCTO4ODKD8iMrycGf5d1K5wMosqbl5TciWi81NcigySgc9g0ALtIi0pdAN25rezZipc7Kq~m6VFjf-KKt9UWRf9Z8tBo3KAGUkqeB6XX2bpm8~yiv~uehnmzbGlAnxyPnLYsvdS9Yw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+          }
+          alt="cohorts"
+          className="md:w-[21rem] md:h-[19.125rem] rounded-xl"
+        />
       </motion.div>
       <div className="text-[#F2F2F2] md:w-[45%] flex flex-col gap-4 text-right ml-auto">
-        <motion.div initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, delay: 0.8 }}>
+        <motion.div
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 0.8 }}
+        >
           <img src={Eye} alt="eye" className="w-8 h-8 ml-auto" />
         </motion.div>
-        <motion.h2 initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, delay: 1 }} className="text-3xl font-medium">
+        <motion.h2
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 1 }}
+          className="text-3xl font-medium"
+        >
           Watch what the whales are doing
         </motion.h2>
-        <motion.p initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, delay: 1.2 }} className="opacity-70 font-medium">
-          All whales are not equal. Know exactly what the whales impacting YOUR portfolio are doing.
+        <motion.p
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 1.2 }}
+          className="opacity-70 font-medium"
+        >
+          All whales are not equal. Know exactly what the whales impacting YOUR
+          portfolio are doing.
         </motion.p>
       </div>
     </section>
@@ -67,8 +108,15 @@ const Section2 = () => {
 
 const Section3 = () => {
   return (
-    <motion.section initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, delay: 1.4 }} className="mt-[1.375rem] sm:px-[3.75rem] px-9">
-      <p className="text-[#f2f2f2] text-right text-2xl font-medium">Testimonials</p>
+    <motion.section
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1, delay: 1.4 }}
+      className="mt-[1.375rem] sm:px-[3.75rem] px-9"
+    >
+      <p className="text-[#f2f2f2] text-right text-2xl font-medium">
+        Testimonials
+      </p>
       <div className="h-[1px] bg-gray-50 mt-5 opacity-50"></div>
     </motion.section>
   );
@@ -77,7 +125,12 @@ const Section3 = () => {
 const Section4 = () => {
   return (
     <section className="mt-10 sm:pl-[3.75rem] pl-9 flex md:flex-row flex-col gap-10 md:items-end">
-      <motion.div initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 1, delay: 1.6 }} className="shrink-0">
+      <motion.div
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1 , delay:1.6 }}
+        className="shrink-0"
+      >
         <img src={Square} alt="" />
       </motion.div>
       <TestimonialsSection />
