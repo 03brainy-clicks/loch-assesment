10a7cb1f9ac5983b5dc08346576deba7b4d68db0
Chandrakant Kushwah <chandrakantkushwah@Chandrakants-MacBook-Air.local>
2023-11-06 12:56:52 +0530

    code and ui review

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -28,8 +28,11 @@ const SignupForm = () => {
 
   return (
     <main className="xl:w-[44.44%] min-h-screen order-0 xl:order-1 p-9 sm:py-16 md:p-0 w-full bg-white flex items-center justify-center">
-      <div className="w-full sm:w-[21.25rem] mx-auto">
-        <motion.h1 {...inputMotion} className="text-[#B0B1B3] text-[2.5rem] font-medium leading-tight">
+      <div className="w-full sm:w-[23.25rem] mx-auto">
+        <motion.h1
+          {...inputMotion}
+          className="text-[#B0B1B3] text-[2.5rem] font-medium leading-tight"
+        >
           Sign up for exclusive access.
         </motion.h1>
         <div className="mt-8">
@@ -39,11 +42,13 @@ const SignupForm = () => {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 type="text"
-                className="py-5 outline-none px-6 input-shadow font-medium w-full rounded-lg border border-gray-100 text-gray-900 placeholder:text-[#CACBCC]"
+                className="py-5 px-6 outline-gray-300 input-shadow font-medium w-full rounded-lg border border-gray-100 text-gray-900 placeholder:text-[#CACBCC]"
                 placeholder="Your email address"
               />
               {error && (
-                <p className="text-red-500 text-xs font-medium leading-4 mt-2">Please enter a valid email.</p>
+                <p className="text-red-500 text-xs font-medium leading-4 mt-2">
+                  Please enter a valid email.
+                </p>
               )}
             </motion.div>
             <motion.div {...inputMotion}>
@@ -58,7 +63,10 @@ const SignupForm = () => {
           </form>
         </div>
         <div className="mt-6">
-          <motion.div {...inputMotion} className="py-[1.125rem] w-full rounded-lg text-center text-[#19191A] font-semibold">
+          <motion.div
+            {...inputMotion}
+            className="py-[1.125rem] w-full rounded-lg text-center text-[#19191A] font-semibold"
+          >
             You’ll receive an email with an invite link to join.
           </motion.div>
         </div>
